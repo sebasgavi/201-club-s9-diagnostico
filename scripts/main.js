c28f4884d60900d5635dd3328927782f5b26a85e
Sebastián Gaviria <sebastiangaviria@outlook.com>
2020-03-31 18:54:12 -0500

    casos con if y else

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,29 @@
 
+// buscar en el html elementos que tengan la clase síntoma
 var sintomas = document.querySelectorAll('.sintoma');
 
+var fiebre = document.querySelector('.fiebre');
+var escalofrios = document.querySelector('.escalofrios');
+var vomito = document.querySelector('.vomito');
+var debilidad = document.querySelector('.debilidad');
+
+// buscar en el html un elemento de tipo button
 var button = document.querySelector('button');
+
+// función a ejecutar cuando el evento se dispare
 function clickEnBoton () {
-  for(let i = 0; i < sintomas.length; i++){
-    console.log(sintomas[i].checked);
-  }
   
+  if(fiebre.checked && escalofrios.checked && vomito.checked){
+    console.log('Diagnóstico: algo peor');
+  } else if(fiebre.checked && escalofrios.checked){
+    console.log('Diagnóstico: algo maluco');
+  }
+
+  // recorrer el arreglo de elementos síntoma
+  //for(let i = 0; i < sintomas.length; i++){
+    // imprimir valor
+    // console.log(sintomas[i].checked);
+  //}
 }
+// creamos un "escuchador" del evento tipo click en el elemento button
 button.addEventListener('click', clickEnBoton);
